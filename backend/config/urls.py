c367358b4a7f7c9746e286dce6c87d603e1643ae
Gregory Savitch <gsavitch@gmail.com>
2026-06-20 15:11:11 -0400

    Add public hosted landing page

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("", lambda request: redirect("card_vault:dashboard"), name="root"),
+    path("", TemplateView.as_view(template_name="landing.html"), name="root"),
     path("admin/", admin.site.urls),
     path("api/card-vault/", include(("card_vault.urls", "card_vault_api"), namespace="card_vault_api")),
     path("card-vault/", include(("card_vault.web_urls", "card_vault"), namespace="card_vault")),
